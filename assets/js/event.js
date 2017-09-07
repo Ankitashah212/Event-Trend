45e8076f549a8f9fb82a1ed5e253c64bc5a8f328
Francisco Carrera <franciscocarrer@gmail.com>
2017-09-07 13:49:38 -0400

    Got ajax response from datausa api

--- assets/js/event.js
+++ assets/js/event.js
@@ -1,5 +1,13 @@
 $(document).ready(function(){
 
-    console.log("Link Test");
+    var queryURL = 'https://api.datausa.io/api?show=geo&sumlevel=nation&required=pop';
+
+    $.ajax({
+        url: queryURL,
+        method: 'GET'
+    }).done(function(response){
+        console.log(response);
+    });
+        
 
 });
