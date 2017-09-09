8ac200f9e95a2258f07120ace8489ffdb77f1821
Francisco Carrera <franciscocarrer@gmail.com>
2017-09-09 01:52:19 -0400

    Added functions to draw counties from d3 website

--- assets/js/event.js
+++ assets/js/event.js
@@ -1,21 +1,33 @@
 $(document).ready(function(){
 
 
-    // Example URL for 
-    var queryURL = 'https://api.datausa.io/api?show=geo&sumlevel=nation&required=pop';
-
-    $.ajax({
-        url: queryURL,
-        method: 'GET'
-    }).done(function(response){
-        console.log(response);
+    var svg = d3.select("svg");
+    
+    var path = d3.geoPath();
+    
+    d3.json("https://d3js.org/us-10m.v1.json", function(error, us) {
+      if (error) throw error;
+    
+      svg.append("g")
+          .attr("fill", "cyan")
+        .selectAll("path")
+        .data(topojson.feature(us, us.objects.states).features)
+        .enter().append("path")
+          .attr("d", path)
+        .append("title")
+          .text(function(d) { return d.id; });
+    
+      svg.append("path")
+          .attr("stroke", "#000")
+          .attr("stroke-width", 0.5)
+          .attr("d", path(topojson.mesh(us, us.objects.counties, function(a, b) { return a !== b; })));
     });
-
-    // Mouse over event for every state that changes the color
-    // Example of what to select for changing state color. 
-    $('.state').on("mouseenter", function(){
-        $(this).find("path").css("fill", "black");
-        console.log("Changing Color", this > 'path');
-    });
-
+    
+    function border(id0, id1) {
+      return function(a, b) {
+        return a.id === id0 && b.id === id1
+            || a.id === id1 && b.id === id0;
+      };
+    }
+    
 });
