429a15b639f6997a703f4a0b42f2990c9ca47ef9
Francisco Carrera <franciscocarrer@gmail.com>
2017-09-10 14:55:51 -0400

    Testing Adding Classes using d3

--- assets/js/event.js
+++ assets/js/event.js
@@ -1,5 +1,22 @@
+function TEST(){
+    $('.county').each( function(){
+        var id = $(this).find('title').html();
+        if (id == 48043){
+            console.log("found county 48043");
+        }
+    });
+}    
+
 $(document).ready(function(){
 
+    var queryURL = 'https://api.datausa.io/api?show=geo&sumlevel=county&required=pop';
+
+    $.ajax({
+        url: queryURL,
+        method: 'GET'
+    }).done(function(response){
+        //console.log(response);
+    });
 
     var svg = d3.select("svg");
     
@@ -7,20 +24,24 @@ $(document).ready(function(){
     
     d3.json("https://d3js.org/us-10m.v1.json", function(error, us) {
       if (error) throw error;
-    
+
       svg.append("g")
           .attr("fill", "cyan")
         .selectAll("path")
-        .data(topojson.feature(us, us.objects.states).features)
-        .enter().append("path")
+        .data(topojson.feature(us, us.objects.counties).features)
+        .enter()
+        .append("path")
           .attr("d", path)
+          .classed("county", true)
         .append("title")
           .text(function(d) { return d.id; });
-    
+
       svg.append("path")
           .attr("stroke", "#000")
           .attr("stroke-width", 0.5)
-          .attr("d", path(topojson.mesh(us, us.objects.counties, function(a, b) { return a !== b; })));
+          .attr("d", path(topojson.mesh(us, us.objects.counties, function(a, b) { 
+            return a !== b;
+            })));
     });
     
     function border(id0, id1) {
@@ -29,5 +50,5 @@ $(document).ready(function(){
             || a.id === id1 && b.id === id0;
       };
     }
-    
+
 });
