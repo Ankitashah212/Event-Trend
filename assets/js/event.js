1a44ada8126728a893cb9836a8b9dcda7b4e163e
Francisco Carrera <franciscocarrer@gmail.com>
2017-09-14 13:23:16 -0400

    Testing Double query using d3 instead of ajax. Looks like it works this time

--- assets/js/event.js
+++ assets/js/event.js
@@ -1,89 +1,47 @@
-var queryURL = 'https://api.datausa.io/api?show=geo&year=latest&sumlevel=county&required=pop';
-var d3URL = 'https://d3js.org/us-10m.v1.json';
-
-var data;
-
-
-var svg = d3.select("svg");
-var path = d3.geoPath();
-
 $(document).ready(function(){
 
-    //////////////
-    // DATAUSA TESTS
-    //////////////
+    var queryURL = 'https://api.datausa.io/api?show=geo&year=latest&sumlevel=county&required=pop';
 
 
-    // $.ajax({
-    //     url: queryURL,
-    //     method: 'GET'
-    // }).done(function(response){
-    //     console.log(response);
-    // });
-
+    var url = "http://api.datausa.io/api/?show=geo&sumlevel=state&required=avg_wage";
+    
     d3.json(queryURL, function(json) {
+    
         var data = json.data.map(function(data){
             return json.headers.reduce(function(obj, header, i){
-            obj[header] = data[i];
-            return obj;
+                obj[header] = data[i];
+                return obj;
             }, {});
         });
+        
         console.log(data);
-    });
 
-    ///////////////
-    // D3 TESTS
-    //////////////
-
+        var svg = d3.select("svg");
+        var path = d3.geoPath();
     
-    // var color = d3.scale.threshold()
-    // .domain([10000, 35000, 65000, 90000, 105000])
-    // .range(["#f2f0f7", "#dadaeb", "#bcbddc", "#9e9ac8", "#756bb1", "#54278f"]);
+        d3.json("https://d3js.org/us-10m.v1.json", function(error, us) {
+        if (error) throw error;
 
-    d3.queue()
-    .defer(d3.json, queryURL)
-    .defer(d3.json, d3URL)
-    .awaitAll(ready);
+        console.log(topojson.feature(us, us.objects.counties).features);
 
-    function ready (error, queryURL, d3URL){
-        if(error) throw error;
+        svg.append("g")
+            .selectAll("path")
+            .data(topojson.feature(us, us.objects.counties).features)
+            .enter()
+        .append("path")
+            .attr("d", path)
+            .attr("fill", "cyan")
+            .classed("county", true)
+        .append("title")
+            .text(function(d) { return d.id; });
 
-        d3.json(d3URL, function (error, us) {
-            if (error) throw error;
-      
-            //console.log(topojson.feature(us, us.objects.counties).features);
-      
-            svg.append("g")
-              .selectAll("path")
-              .data(topojson.feature(us, us.objects.counties).features)
-              .enter()
-              .append("path")
-                .attr("d", path)
-                .classed("county", true)
-              .append("title")
-                .text(function(d) { return d.id; });
-      
-              // svg.append("g")
-              //     .selectAll("path")
-              //     .data(data)
-              //     .enter()
-              // .append("path")
-              //     .style("fill", function(d) { return color(rateById[d.id]); });
-       
-            svg.append("path")
-                .attr("stroke", "#000")
-                .attr("stroke-width", 0.5)
-                .attr("d", path(topojson.mesh(us, us.objects.counties, function(a, b) { 
-                  return a !== b;
+        svg.append("path")
+            .attr("stroke", "#000")
+            .attr("stroke-width", 0.5)
+            .attr("d", path(topojson.mesh(us, us.objects.counties, function(a, b) { 
+                return a !== b;
             })));
         });
-    }
 
-    function border(id0, id1) {
-      return function(a, b) {
-        return a.id === id0 && b.id === id1
-            || a.id === id1 && b.id === id0;
-      };
-    }
-
+    });
 });
