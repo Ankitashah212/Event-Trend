1434986e7ccd61b0e4158fc2681e7c8f9a647582
Francisco Carrera <franciscocarrer@gmail.com>
2017-09-14 12:42:52 -0400

    Failed way to do queue requests, do not replicate

--- assets/js/event.js
+++ assets/js/event.js
@@ -1,49 +1,84 @@
-function TEST(){
-    $('.county').each( function(){
-        var id = $(this).find('title').html();
-        if (id == 48043){
-            console.log("found county 48043");
-        }
-    });
-}    
+var queryURL = 'https://api.datausa.io/api?show=geo&year=latest&sumlevel=county&required=pop';
+var d3URL = 'https://d3js.org/us-10m.v1.json';
+
+var data;
+
+
+var svg = d3.select("svg");
+var path = d3.geoPath();
 
 $(document).ready(function(){
 
-    var queryURL = 'https://api.datausa.io/api?show=geo&sumlevel=county&required=pop';
+    //////////////
+    // DATAUSA TESTS
+    //////////////
 
-    $.ajax({
-        url: queryURL,
-        method: 'GET'
-    }).done(function(response){
-        //console.log(response);
+
+    // $.ajax({
+    //     url: queryURL,
+    //     method: 'GET'
+    // }).done(function(response){
+    //     console.log(response);
+    // });
+
+    d3.json(queryURL, function(json) {
+        var data = json.data.map(function(data){
+            return json.headers.reduce(function(obj, header, i){
+            obj[header] = data[i];
+            return obj;
+            }, {});
+        });
+        console.log(data);
     });
 
-    var svg = d3.select("svg");
-    
-    var path = d3.geoPath();
-    
-    d3.json("https://d3js.org/us-10m.v1.json", function(error, us) {
-      if (error) throw error;
+    ///////////////
+    // D3 TESTS
+    //////////////
 
-      svg.append("g")
-          .attr("fill", "cyan")
-        .selectAll("path")
-        .data(topojson.feature(us, us.objects.counties).features)
-        .enter()
-        .append("path")
-          .attr("d", path)
-          .classed("county", true)
-        .append("title")
-          .text(function(d) { return d.id; });
+    
+    // var color = d3.scale.threshold()
+    // .domain([10000, 35000, 65000, 90000, 105000])
+    // .range(["#f2f0f7", "#dadaeb", "#bcbddc", "#9e9ac8", "#756bb1", "#54278f"]);
 
-      svg.append("path")
-          .attr("stroke", "#000")
-          .attr("stroke-width", 0.5)
-          .attr("d", path(topojson.mesh(us, us.objects.counties, function(a, b) { 
-            return a !== b;
+    d3.queue()
+    .defer(d3.json, queryURL)
+    .defer(d3.json, d3URL)
+    .awaitAll(ready);
+
+    function ready (error, queryURL, d3URL){
+        if(error) throw error;
+
+        d3.json(d3URL, function (error, us) {
+            if (error) throw error;
+      
+            //console.log(topojson.feature(us, us.objects.counties).features);
+      
+            svg.append("g")
+              .selectAll("path")
+              .data(topojson.feature(us, us.objects.counties).features)
+              .enter()
+              .append("path")
+                .attr("d", path)
+                .classed("county", true)
+              .append("title")
+                .text(function(d) { return d.id; });
+      
+              // svg.append("g")
+              //     .selectAll("path")
+              //     .data(data)
+              //     .enter()
+              // .append("path")
+              //     .style("fill", function(d) { return color(rateById[d.id]); });
+       
+            svg.append("path")
+                .attr("stroke", "#000")
+                .attr("stroke-width", 0.5)
+                .attr("d", path(topojson.mesh(us, us.objects.counties, function(a, b) { 
+                  return a !== b;
             })));
-    });
-    
+        });
+    }
+
     function border(id0, id1) {
       return function(a, b) {
         return a.id === id0 && b.id === id1
